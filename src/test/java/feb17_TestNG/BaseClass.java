695d6c104b1655f514f48241f474eebbb6f14678
SURAJEG1 <surajpatil.eg@gmail.com>
2026-02-18 01:05:45 +0530

    5th commit

--- src/test/java/feb17_TestNG/BaseClass.java
+++ src/test/java/feb17_TestNG/BaseClass.java
@@ -1,5 +1,43 @@
 package feb17_TestNG;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
 public class BaseClass {
+	
+	ReadConfig readconfig = new ReadConfig();
+	String browser = readconfig.getBrowser();
+	String amazonUrl = readconfig.getAmazonUrl();
+	String facebookUrl = readconfig.getFacebookUrl();
+	
+
+	
+	public static WebDriver driver;
+	@BeforeClass
+	public void setup() 
+	{
+		switch(browser.toLowerCase()) 
+		{
+		case"chrome":
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		break;
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/test/java/feb17_TestNG/ReadConfig.java
+++ src/test/java/feb17_TestNG/ReadConfig.java
@@ -1,5 +1,71 @@
 package feb17_TestNG;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadConfig {
+
+	Properties properties;
+	String path = "E:\\InfoEge Automation Project\\Feb_26-Practice_ArtifactID\\Resources\\config.properties";
+
+	public ReadConfig() 
+	{
+		try {
+			
+			properties = new Properties();
+			FileInputStream fis = new FileInputStream(path);
+			properties.load(fis);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+
+//Method for browser Key		
+	public String getBrowser() 
+	{
+		String value = properties.getProperty("browser");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("Browser is not spacified in configfile");
+	}
+	
+	
+	
+	
+//Method for Url Key	
+	public String getAmazonUrl() 
+	{
+		String value = properties.getProperty("amazonUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("Url is not spacified in configfile");
+	}
+	
+	
+	
+	
+	public String getFacebookUrl() 
+	{
+		String value = properties.getProperty("facebookUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("Url is not spacified in configfile");
+	}
+	
+	
+	
+
+	
+
+
 
 }
