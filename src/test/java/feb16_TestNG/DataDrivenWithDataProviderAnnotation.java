278981d65ec8fc8eff6cefbc07556099e2d8bdb9
SURAJEG1 <surajpatil.eg@gmail.com>
2026-02-17 20:43:45 +0530

    4th commit

--- src/test/java/feb16_TestNG/DataDrivenWithDataProviderAnnotation.java
+++ src/test/java/feb16_TestNG/DataDrivenWithDataProviderAnnotation.java
@@ -1,7 +1,16 @@
 package feb16_TestNG;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -9,18 +18,25 @@ public class DataDrivenWithDataProviderAnnotation {
 	
 	WebDriver driver;
 	
-	@BeforeClass
+	@BeforeMethod
 	public void setup() 
 	{
-		
+		ChromeOptions co = new ChromeOptions();
+		co.addArguments("--incognito");
+		driver = new ChromeDriver(co);
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 	
 	
 	
 	@Test(dataProvider = "credentialDetails")
-	public void loginTest001(String un, String pwd) 
+	public void loginTest001(String product) 
 	{
-		
+		WebElement searchBox = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		searchBox.sendKeys(product);
+		searchBox.sendKeys(Keys.ENTER);
 	}
 	
 	
@@ -40,8 +56,11 @@ public class DataDrivenWithDataProviderAnnotation {
 	
 	
 	
-	
-	
+	@AfterMethod
+	public void tearDown() 
+	{
+		driver.quit();
+	}
 	
 	
 	
---
+++ src/test/java/feb17_TestNG/ReadConfig.java
@@ -0,0 +1,5 @@
+package feb17_TestNG;
+
+public class ReadConfig {
+
+}
