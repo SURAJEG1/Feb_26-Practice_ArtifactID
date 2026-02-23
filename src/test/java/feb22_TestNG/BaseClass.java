45c6ea5cba73273133ad713f35506efed7b8337c
SURAJEG1 <surajpatil.eg@gmail.com>
2026-02-23 15:48:17 +0530

    set browser key and Url key in config.properties file and create readconfig class or baseclass

--- src/test/java/feb22_TestNG/BaseClass.java
+++ src/test/java/feb22_TestNG/BaseClass.java
@@ -1,5 +1,64 @@
 package feb22_TestNG;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class BaseClass {
+
+	ReadConfig readconfig;
+	String amazonUrl = readconfig.getAmazonUrl();
+	String facebookUrl = readconfig.getFacebookUrl();
+	String flipkartUrl = readconfig.getFlipkartUrl();
+	String rediffmailUrl = readconfig.getRediffmailUrl();
+	String orangehrmsUrl = readconfig.getOrangehrmUrl();
+	String chromeBrowser = readconfig.getBrowser();
+
+	public static WebDriver driver;
+
+	@BeforeClass
+	public void setup() 
+	{
+		switch(chromeBrowser.toLowerCase()) 
+		{
+		case "chrome":
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			break;
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+
+
+
+
+
+
+	@AfterClass
+	public void tearDown() 
+	{
+		driver.close();
+		driver.quit();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- src/test/java/feb22_TestNG/ReadConfig.java
+++ src/test/java/feb22_TestNG/ReadConfig.java
@@ -1,5 +1,107 @@
 package feb22_TestNG;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadConfig {
+	
+	Properties properties;
+	String path = "config.properties";
+	
+	//Constructor
+	public ReadConfig() 
+	{
+		try {
+			
+		properties = new Properties();
+		FileInputStream fis = new FileInputStream(path);
+		properties.load(fis);
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	// Create methods for set URL key and browser key.  
+	
+	public String getBrowser() 
+	{
+		String value = properties.getProperty("browser");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("Browser is not spacified in configFile");
+	}
+	
+	
+	public String getAmazonUrl() 
+	{
+		String value = properties.getProperty("amazonUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("amazonUrl is not spacified in configFile");
+	}
+	
+	
+	public String getFacebookUrl() 
+	{
+		String value = properties.getProperty("facebookUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("facebookUrl is not spacified in configFile");
+	}
+	
+	
+	public String getFlipkartUrl() 
+	{
+		String value = properties.getProperty("flipkartUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("flipkartUrl is not spacified in configFile");
+	}
+	
+	
+	public String getRediffmailUrl() 
+	{
+		String value = properties.getProperty("rediffmailUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("rediffmailUrl is not spacified in configFile");
+	}
+	
+	
+	public String getOrangehrmUrl() 
+	{
+		String value = properties.getProperty("orangehrmUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("orangehrmUrl is not spacified in configFile");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
