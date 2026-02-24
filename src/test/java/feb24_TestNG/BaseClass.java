416037e3bf6ddc09099c1f3027625eb92984d709
SURAJEG1 <surajpatil.eg@gmail.com>
2026-02-25 00:43:18 +0530

    feb24_TestNG_2nd Commit

--- src/test/java/feb24_TestNG/BaseClass.java
+++ src/test/java/feb24_TestNG/BaseClass.java
@@ -1,5 +1,55 @@
 package feb24_TestNG;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class BaseClass {
+	
+	//Create Objects
+	ReadConfig readconfig;
+	String baseUrl = readconfig.getBaseUrl();
+	String browser = readconfig.getBrowser();
+	
+	public static WebDriver driver;
+	@BeforeClass
+	public void setup() 
+	{
+		switch(browser.toLowerCase()) 
+		{
+		case "chrome":
+			ChromeOptions co = new ChromeOptions();
+			co.addArguments("--incognito --start-Maximized");
+			driver = new ChromeDriver(co);
+			break;
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+	
+	
+	@AfterClass
+	public void tearDown() 
+	{
+		driver.close();
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
--- src/test/java/feb24_TestNG/ReadConfig.java
+++ src/test/java/feb24_TestNG/ReadConfig.java
@@ -1,5 +1,64 @@
 package feb24_TestNG;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadConfig {
+	//Crate Objects
+	Properties properties;
+	String path = "";
+
+	//Constructor
+	public ReadConfig() 
+	{
+		try {
+			properties = new Properties();
+			FileInputStream fis = new FileInputStream(path);
+			properties.load(fis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	//Create method for Url key
+	public String getBaseUrl() 
+	{
+		String value = properties.getProperty("baseUrl");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("Url is not spacified in configfile");
+	}
+
+
+
+
+//Create method for browser key
+	public String getBrowser() 
+	{
+		String value = properties.getProperty("browser");
+		if(value!=null)
+			return value;
+		else
+			throw new RuntimeException("Browser is not spacified in configfile");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
